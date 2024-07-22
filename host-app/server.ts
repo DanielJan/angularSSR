fbeba42c25e846d5ca52ad8043cadb35bde36a4e
Daniel Janik <daniel.janik@bluesoft.com>
2024-07-22 11:53:36 +0200

    angular universal-ssr przed migracja v5 node:fs angular 17

--- host-app/server.ts
+++ host-app/server.ts
@@ -1,69 +1,55 @@
-// // declare const __non_webpack_require__: NodeRequire;
 
-// const mainModule = __non_webpack_require__.main;
-// const moduleFilename = mainModule?.filename || '';
-// if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-//   import('./server-bootstrap.ts').then(x => x.run());
-// }
+import 'zone.js/node';
 
-import "zone.js/node";
-import "@angular/compiler";
-
-import { ngExpressEngine } from "@nguniversal/express-engine";
-import * as express from "express";
-import { join } from "path";
-
-import { AppServerModule } from "./src/main.server";
-import { APP_BASE_HREF } from "@angular/common";
-import "@angular/compiler";
-import { existsSync } from "fs";
+import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine } from '@angular/ssr';
+import * as express from 'express';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app() {
+export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), "dist");
-  const indexHtml = existsSync(join(distFolder, "index.original.html"))
-    ? "index.original.html"
-    : "index";
+  const distFolder = join(process.cwd(), 'dist/host-app/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? join(distFolder, 'index.original.html')
+    : join(distFolder, 'index.html');
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine(
-    "html",
-    ngExpressEngine({
-      bootstrap: AppServerModule,
-    })
-  );
+  const commonEngine = new CommonEngine();
 
-  server.set("view engine", "html");
-  server.set("views", distFolder);
-
-  server.get("/favicon.ico", (req, res) => {
-    res.status(404).send();
-  });
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get(
-    "*.*",
-    express.static(distFolder, {
-      maxAge: "1y",
-    })
-  );
+  server.get('*.*', express.static(distFolder, {
+    maxAge: '1y'
+  }));
 
-  // All regular routes use the Universal engine
-  server.get("*", (req, res) => {
-    res.render(indexHtml, {
-      req,
-      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
-    });
+  // All regular routes use the Angular engine
+  server.get('*', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: distFolder,
+        providers: [
+          { provide: APP_BASE_HREF, useValue: baseUrl },],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
   });
 
   return server;
 }
 
-export function run() {
-  const port = process.env.PORT || 4000;
+function run(): void {
+  const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
   const server = app();
@@ -72,4 +58,14 @@ export function run() {
   });
 }
 
-export * from "./src/main.server";
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
+
+export default bootstrap;
